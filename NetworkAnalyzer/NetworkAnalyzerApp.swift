0b823bee87b452106e5bad99bb5311c8e595937f
Matt Safeme <mattsafeme@gmail.com>
2025-12-30 23:14:01 -0800

    - Initial commit

---
+++ NetworkAnalyzer/NetworkAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkAnalyzerApp.swift
+//  NetworkAnalyzer
+//
+//  Created by s on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
